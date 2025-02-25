f183b1a79f1116b844c9785166d7b6edcf38aaca
Jaydeep Mondal <mjay00217@gmail.com>
2025-02-25 06:34:23 +0530

    Day 2

--- Java/patterns/butterfly.java
+++ Java/patterns/butterfly.java
@@ -1,18 +1,32 @@
-// *      *
-// **    **
-// ***  ***
-// ********
-// ***  ***
-// **    **
-// *      *
-//butterfly pattern
-
-
-
 package patterns;
 
 public class butterfly {
-    public static void butter(int h){
-        for(int )
+    public static void butter(int h) {
+        // Upper Half
+        for (int i = 1; i <= h; i++) {
+            for (int j = 1; j <= i; j++)
+                System.out.print("* ");
+            for (int j = 1; j <= ((h - i) * 2); j++)
+                System.out.print("  ");
+            for (int j = 1; j <= i; j++)
+                System.out.print("* ");
+            System.out.println();
+        }
+
+        // Lower Half
+        for (int i = h - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++)
+                System.out.print("* ");
+            for (int j = 1; j <= ((h - i) * 2); j++)
+                System.out.print("  ");
+            for (int j = 1; j <= i; j++)
+                System.out.print("* ");
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int height = 4; 
+        butter(height);
     }
 }
