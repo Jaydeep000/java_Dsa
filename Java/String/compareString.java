012bb2ef28fd97f4be6c8cd07c7963485de977b6
Jaydeep Mondal <mjay00217@gmail.com>
2025-03-07 01:54:25 +0530

    addon

---
+++ Java/String/compareString.java
@@ -0,0 +1,23 @@
+package String;
+
+public class compareString {
+
+    public static void subString(){
+        
+    }
+    public static void main(String[] args) {
+        // String s1="Tony";
+        // String s2="Tony";
+        // String s3=new String("Tony");
+
+        // if(s1==s3)
+        //     System.out.println("True");
+        // else System.out.println("False");
+
+        // if(s1.equals(s3))
+        //     System.out.println("True");
+        // else System.out.println("False");
+        
+    }
+}
+
